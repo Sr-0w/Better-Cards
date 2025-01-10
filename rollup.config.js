5eabe0b7e1a2623e6f4beac847b420456779b332
Sr-0w <139899120+Sr-0w@users.noreply.github.com>
2025-01-10 16:56:34 +0100

    Create rollup.config.js

---
+++ rollup.config.js
@@ -0,0 +1,37 @@
+import babel from '@rollup/plugin-babel';
+import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
+import nodeResolve from '@rollup/plugin-node-resolve';
+import terser from '@rollup/plugin-terser';
+import serve from 'rollup-plugin-serve';
+
+const dev = process.env.ROLLUP_WATCH;
+
+const serveOpts = {
+  contentBase: ['./dist'],
+  host: '0.0.0.0',
+  port: 5000,
+  allowCrossOrigin: true,
+  headers: {
+    'Access-Control-Allow-Origin': '*',
+  },
+};
+
+export default {
+  input: 'src/smart-light-card.js',
+  output: {
+    dir: 'dist',
+    format: 'es',
+  },
+  plugins: [
+    nodeResolve(),
+    commonjs(),
+    json(),
+    babel({
+      babelHelpers: 'bundled',
+      exclude: 'node_modules/**',
+    }),
+    !dev && terser(),
+    dev && serve(serveOpts),
+  ],
+};
