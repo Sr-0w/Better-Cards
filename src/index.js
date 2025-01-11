333aaa34212a6cedf570781eee5c7756eff39e29
Sr-0w <139899120+Sr-0w@users.noreply.github.com>
2025-01-11 12:42:04 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,4 @@
-import './cards/switch/better-switch-card';
-// Import future cards here
-// import './cards/light/better-light-card';
-// import './cards/sensor/better-sensor-card';
+import { log } from './shared/utils';
+import './cards/switch';
 
-console.info(
-  '%c BETTER CARDS %c 1.0.0 ',
-  'color: white; background: #555; font-weight: 700;',
-  'color: white; background: #000; font-weight: 700;',
-);
+log.info('Better Cards loaded');
