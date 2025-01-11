2ec3ea2eaba237aa67116a9fa348fe0454dd15b6
Sr-0w <139899120+Sr-0w@users.noreply.github.com>
2025-01-12 00:17:24 +0100

    Update const.js

--- src/cards/switch/const.js
+++ src/cards/switch/const.js
@@ -1,9 +1,11 @@
 export const CARD_VERSION = '1.0.0';
 export const CARD_NAME = 'Better Switch Card';
 
+export const DOMAINS = ['switch', 'light', 'input_boolean'];
+
 export const DEFAULT_CONFIG = {
   entity: '',
   name: '',
-  show_slider: false,
+  icon: '',
   animation_duration: 500,
 };
