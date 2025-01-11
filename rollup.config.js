977fadd93e298f8c41d721e3e1cfabf99171eaff
Sr-0w <139899120+Sr-0w@users.noreply.github.com>
2025-01-11 09:08:59 +0100

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import babel from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
-import nodeResolve from '@rollup/plugin-node-resolve';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 import terser from '@rollup/plugin-terser';
 import serve from 'rollup-plugin-serve';
 
@@ -33,5 +33,5 @@ export default {
     }),
     !dev && terser(),
     dev && serve(serveOpts),
-  ],
+  ].filter(Boolean),
 };
