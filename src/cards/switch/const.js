ea61045b0c307aa8e4857b07654bfe31c8f6fda3
Sr-0w <139899120+Sr-0w@users.noreply.github.com>
2025-01-12 02:21:08 +0100

    Update const.js

--- src/cards/switch/const.js
+++ src/cards/switch/const.js
@@ -1,6 +1,5 @@
 export const CARD_VERSION = '1.0.0';
 export const CARD_NAME = 'Better Switch Card';
-
 export const DOMAINS = ['switch', 'light', 'input_boolean'];
 
 export const DEFAULT_CONFIG = {
@@ -8,4 +7,12 @@ export const DEFAULT_CONFIG = {
   name: '',
   icon: '',
   animation_duration: 500,
+};
+
+// Add validation schema if needed
+export const configStruct = {
+  entity: String,
+  name: String,
+  icon: String,
+  animation_duration: Number,
 };
