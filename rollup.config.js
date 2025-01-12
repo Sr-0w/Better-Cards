cd470a1b018d486fc436d2bf46f42116c413d649
Sr-0w <139899120+Sr-0w@users.noreply.github.com>
2025-01-12 03:14:42 +0100

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,48 +1,28 @@
 import babel from '@rollup/plugin-babel';
-import commonjs from '@rollup/plugin-commonjs';
-import json from '@rollup/plugin-json';
-import { nodeResolve } from '@rollup/plugin-node-resolve';
-import terser from '@rollup/plugin-terser';
-import serve from 'rollup-plugin-serve';
-
-const dev = process.env.ROLLUP_WATCH;
-
-const serveOpts = {
-  contentBase: ['./dist'],
-  host: '0.0.0.0',
-  port: 5000,
-  allowCrossOrigin: true,
-  headers: {
-    'Access-Control-Allow-Origin': '*',
-  },
-};
+import typescript from '@rollup/plugin-typescript'; // Add TypeScript plugin
 
 export default {
   input: 'src/index.js',
   output: {
     file: 'dist/better-cards.js',
-    format: 'es',
-    sourcemap: true,
+    format: 'umd',
+    name: 'BetterCards',
   },
   plugins: [
-    nodeResolve(),
-    commonjs(),
-    json(),
+    typescript(),  // Use the TypeScript plugin to handle TS files
     babel({
       babelHelpers: 'bundled',
       exclude: 'node_modules/**',
       presets: [
-        '@babel/preset-env', // For modern JS
-        '@babel/preset-typescript', // If using TypeScript
+        '@babel/preset-env',
+        '@babel/preset-typescript', // Add preset for TypeScript
       ],
       plugins: [
-        ['@babel/plugin-proposal-decorators', { legacy: true }], // Enable decorators
-        ['@babel/plugin-transform-class-properties', { loose: true }], // Make sure 'loose' mode is true
-        ['@babel/plugin-transform-private-methods', { loose: true }], // Same for private methods
-        ['@babel/plugin-transform-private-property-in-object', { loose: true }], // And for private property accessors
+        ['@babel/plugin-proposal-decorators', { legacy: true }],
+        '@babel/plugin-proposal-class-properties',
+        '@babel/plugin-proposal-private-methods',
+        '@babel/plugin-proposal-private-property-in-object',
       ],
     }),
-    !dev && terser(),
-    dev && serve(serveOpts),
-  ].filter(Boolean),
+  ],
 };
