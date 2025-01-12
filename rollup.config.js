b0688f33d8bec8938204b207292362a559175a47
Sr-0w <139899120+Sr-0w@users.noreply.github.com>
2025-01-12 12:42:26 +0100

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,6 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import terser from '@rollup/plugin-terser';
-import babel from '@rollup/plugin-babel';
-import json from '@rollup/plugin-json';
 
 export default {
   input: 'src/index.js',
@@ -17,17 +15,6 @@ export default {
       preferBuiltins: false,
     }),
     commonjs(),
-    babel({
-      babelHelpers: 'bundled',
-      exclude: 'node_modules/**',
-      presets: [
-        ['@babel/preset-env', { 
-          targets: 'defaults',
-          modules: false 
-        }]
-      ]
-    }),
-    json(),
     terser()
   ],
   external: [
