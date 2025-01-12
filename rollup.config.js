f2b92dfbb87d47d3642f191443ba4c5eba1122d7
Sr-0w <139899120+Sr-0w@users.noreply.github.com>
2025-01-12 12:34:47 +0100

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,28 +1,34 @@
-import babel from '@rollup/plugin-babel';
-import typescript from '@rollup/plugin-typescript'; // Add TypeScript plugin
+import typescript from '@rollup/plugin-typescript';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import terser from '@rollup/plugin-terser';
 
 export default {
   input: 'src/index.js',
   output: {
-    file: 'dist/better-cards.js',
-    format: 'umd',
-    name: 'BetterCards',
+    dir: 'dist',
+    format: 'es',
+    sourcemap: true,
+    name: 'BetterCards'
   },
   plugins: [
-    typescript(),  // Use the TypeScript plugin to handle TS files
-    babel({
-      babelHelpers: 'bundled',
-      exclude: 'node_modules/**',
-      presets: [
-        '@babel/preset-env',
-        '@babel/preset-typescript', // Add preset for TypeScript
-      ],
-      plugins: [
-        ['@babel/plugin-proposal-decorators', { legacy: true }],
-        '@babel/plugin-proposal-class-properties',
-        '@babel/plugin-proposal-private-methods',
-        '@babel/plugin-proposal-private-property-in-object',
-      ],
+    resolve({
+      browser: true,
+      preferBuiltins: false,
     }),
+    commonjs(),
+    typescript({
+      tsconfig: './tsconfig.json',
+      sourceMap: true,
+      inlineSources: true,
+      declaration: true,
+      declarationDir: './dist/types'
+    }),
+    terser()
   ],
+  external: [
+    'lit',
+    'lit/decorators.js',
+    'custom-card-helpers'
+  ]
 };
